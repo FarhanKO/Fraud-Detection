0695b3b9bf0f2f41708cca5b713efe57c3d345bc
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-07-11 16:20:18 +0600

    Create app.py

---
+++ Fraud Detection/app/app.py
@@ -0,0 +1,9 @@
+import sys
+from pathlib import Path
+ 
+import joblib
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from sklearn.preprocessing import RobustScaler
